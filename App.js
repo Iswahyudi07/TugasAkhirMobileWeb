e0deb22563404a3c39e4c46f813ccd29dbd45d1f
Iswahyudi <iswahyudi352@gmail.com>
2021-01-22 09:23:59 +0700

    https://github.com/Iswahyudi07/tugasakhir

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Beranda, Input, Edit } from './src/screens';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Beranda" component={Beranda} /> */}
+        <Stack.Screen
+          name="Beranda"
+          component={Beranda}
+          options={({ navigation: { navigate } }) => ({
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigate('Input', {
+                  tipe: 'add',
+                })}
+                style={{ padding: 10, justifyContent: 'flex-end' }}
+              >
+                <Ionicons name="md-add-circle" size={40} color="salmon" />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Stack.Screen name="Input" component={Input} />
+        <Stack.Screen name="Edit" component={Edit} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
